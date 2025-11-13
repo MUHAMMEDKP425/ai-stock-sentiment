ee875eea7d56957f34d6ef5fd49772122ddd6dd7
MUHAMMEDKP425 <kpm96608@gmail.com>
2025-11-13 21:41:02 +0530

    Create stock_list.py

---
+++ stock_list.py
@@ -0,0 +1,14 @@
+stock_symbols = {
+    "AAPL": "Apple Inc.",
+    "GOOG": "Alphabet Inc.",
+    "MSFT": "Microsoft Corporation",
+    "TSLA": "Tesla Inc.",
+    "META": "Meta Platforms",
+    "AMZN": "Amazon.com Inc.",
+    "NFLX": "Netflix Inc.",
+    "NVDA": "NVIDIA Corporation",
+    "TCS": "Tata Consultancy Services",
+    "TATAMOTORS": "Tata Motors",
+    "RELIANCE": "Reliance Industries",
+    "HDFCBANK": "HDFC Bank",
+}
