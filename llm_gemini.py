c877f1561b195a072b1ebbbb39da6d75e031abc7
MUHAMMEDKP425 <kpm96608@gmail.com>
2025-11-13 21:08:16 +0530

    Update llm_gemini.py

--- llm_gemini.py
+++ llm_gemini.py
@@ -1,14 +1,25 @@
-
 import google.generativeai as genai
 import os
 
-def summarize_and_sentiment(text):
+def summarize_and_sentiment_gemini(text):
     api_key = os.getenv("GEMINI_API_KEY")
-    if not api_key:
-        raise Exception("No GEMINI_API_KEY found")
 
+    if not api_key:
+        raise Exception("No GEMINI_API_KEY found in Streamlit Secrets.")
+
+    # Configure Gemini
     genai.configure(api_key=api_key)
+
     model = genai.GenerativeModel("gemini-2.0-flash")
-    prompt = f"Summarize this news and predict its sentiment: {text}"
+
+    prompt = f"""
+    Summarize this stock news in 2 sentences.
+    Then give sentiment: positive, neutral, or negative.
+    Then give one short reason explaining why.
+
+    News:
+    {text}
+    """
+
     response = model.generate_content(prompt)
-    return {"raw": response.text}
+    return response.text
